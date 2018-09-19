0d3b6f9c5d29431157beb9b84f13c5b314418872
Andreas Alme <andreas.alme@apper.se>
2018-09-19 13:56:11 +0200

    setup eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Login = props => {
+  return <div />;
+};
 
 class App extends Component {
   render() {
@@ -13,9 +17,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Login />
       </div>
     );
   }
 }
 
+// if a module defines a default export
+// you can import that default export without the curly braces: import foo from 'foo'
 export default App;
